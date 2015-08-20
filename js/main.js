12f873a26c0059644812137d8ce7fe26af1fadf5
vinceg <vincegiorno324@gmail.com>
2015-08-19 20:33:44 -0400

    Add jQuery IIF wrapper, render->render()

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-
+$(function(){
 
 var Totals = Backbone.Model.extend ({
     defaults: function() {
       return {
         calories: 0,
         sodium: 0,
-        fat: 0,
+        totFat: 0,
         satFat: 0
       };
     }
@@ -17,11 +17,22 @@ var TotalsView = Backbone.View.extend ({
     el: '#table-div',
     template: _.template($('#totals-template').html()),
     events: {
-        "click #search-btn": "handleSearch",
-        "keypress  
-    }
+        "click #search-btn": "search",
+        "keypress": "searchOnEnter"  
+    },
     initialize: function(){
-        this.render;
+        this.render();   
+        this.listenTo(this.model, 'change', this.render);
+    },
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    },
+    updateOnEnter: function(e) {
+      if (e.keyCode == 13) this.search();
+    },
+    search: function() {
+        console.log('search called');
     }
 });
 
@@ -29,3 +40,5 @@ var TotalsView = Backbone.View.extend ({
 
 
 var totalsView = new TotalsView({model: totals});
+    
+});
